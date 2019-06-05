800d73786b04775755c5a360e162a8b7ac85cc61
La Monte Henry Piggy Yarroll <piggy.yarroll@gmail.com>
2019-06-05 09:28:58 -0400

    Add a hash-based tokenizer.
    This is about twice as fast as the regex-based tokenizer if we are
    just matching space-separated tokens.

--- tokenizer.py
+++ tokenizer.py
@@ -3,14 +3,14 @@
 import abc
 import csv
 import re
-from typing import Any, Iterable, List, Optional
+from typing import Any, Iterable, List, Optional, Set, Union
 
 class Tokenizer(object):
 
     _metaclass_ = abc.ABCMeta
     _file = ...  # type: Optional[Iterable[str]]
     _data = ...  # type: Optional[Iterable[str]]
-    _pattern = ...  # type: re._pattern_type
+    _pattern = ...  # type: Union[re._pattern_type, Set[str]]:
     # These go at the beginning of the regex.
     _extra_regex = ...  # type: List[str]
     _filename = ...  # type: Optional[str]
@@ -24,15 +24,19 @@ class Tokenizer(object):
             self._file = None
         self._data = data
 
-        self._pattern = '|'.join(
-            self._extra_regex + 
-            sorted(set([self.make_pattern(word) for word in self.read_records()]), reverse=True))
+        self._pattern = self.build_pattern()
 
         self._file.close()
 
+    def build_pattern(self) -> Union[re._pattern_type, Set[str]]:
+        return '|'.join(
+            self._extra_regex +
+            sorted(set([self.make_pattern(word) for word in self.read_records()]),
+                   reverse=True))
+
     def contents(self):
         return self._file or self._data
-    
+
     @abc.abstractmethod
     def read_records(self) -> Iterable[str]:
         """Generator that returns strings from self.contents()"""
@@ -63,3 +67,31 @@ class Tokenizer(object):
                 line = line[ws.end():]
             yield (match, line)
             match = self.match(line)
+
+
+class HashTokenizer(Tokenizer):
+
+    def build_pattern(self):
+        return set(self.read_records())
+
+    def make_pattern(self, word: str) -> str:
+        """Convert word into a pattern fragment."""
+        return word
+
+    def tokenize(self, line: str) -> (Any, str):
+        """Consume tokens that match the pattern.
+
+        Also consumes whitespace.
+        Args:
+          line - The input fragment to tokenize.
+        Returns:
+          (matched token (str), unmatched remainder)
+        """
+        tokens = line.split()
+        while tokens:
+            if tokens[0] in self._pattern:
+                retval = tokens.pop(0)
+                yield(retval, ' '.join(tokens))
+            else:
+                yield(None, ' '.join(tokens))
+                break
