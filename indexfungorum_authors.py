ea5318ee286ec7a4d72d0f6723aacc72baa42389
La Monte Henry Piggy Yarroll <piggy.yarroll@gmail.com>
2019-06-06 13:34:02 -0400

    Extra author abbreviations from the Index Fungorum author list.
    http:/www.indexfungorum.org/FungalNameAuthors.rtf
    unrtf --text FungalNameAuthors.rtf | iconv -f ISO-8859-15 -t utf-8 > FungalNameAuthors.txt

---
+++ indexfungorum_authors.py
@@ -0,0 +1,76 @@
+"""Process the author list from Index Fungorum.
+
+This includes author abbreviations, unlike the MycoBank data.
+
+$ wget http:/www.indexfungorum.org/FungalNameAuthors.rtf
+$ unrtf --text FungalNameAuthors.rtf | iconv -f ISO-8859-15 -t utf-8 > FungalNameAuthors.txt
+"""
+
+import argparse
+import csv
+import re
+import sys
+from typing import Iterator, List, Optional
+
+import tokenizer
+
+class IndexFungorumAuthors(tokenizer.HashTokenizer):
+
+    _filename = 'data/authors/FungalNameAuthors.txt'
+
+    _extra_words: List[str] = ['ex', 'in']
+
+    def split(self, line: str) -> List[str]:
+        return line.translate(str.maketrans(',[]()-', '      ')).split()
+
+    def read_records(self) -> Iterator[str]:
+        for word in self._extra_words:
+            yield(word)
+        start = True
+        for line in self.contents():
+            if start:
+                if line == '-----------------\n':
+                    start = False
+                continue
+            # Remove trailing publication data and forename notation.
+            line = re.sub(r'\([^)]*\)$|\[no forename[s]*\]', '', line.lower().strip())
+            # Catch alternative spellings.
+            m = re.match(r'\((.*)\) see (.*)', line)
+            if m:
+                line = ' '.join(m.groups())
+            line = line.translate(str.maketrans(',[]()-', '      '))
+            for name in line.split():
+                yield(name)
+                if '.' in name:
+                    for initial in name.split('.'):
+                        if initial:
+                            yield(initial + '.')
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('file',
+                        type=str,
+                        help='filename of the UTF-8 csv MycoBank Export')
+    parser.add_argument('names',
+                        nargs='*',
+                        type=str,
+                        help='list of names to check for --tokenize')
+    parser.add_argument('--tokenize',
+                        help='Process tokens from the command line.',
+                        action='store_true')
+    args = parser.parse_args()
+
+    indexfungorum = IndexFungorumAuthors(args.file)
+
+    if args.tokenize:
+        for name in args.names:
+            for (m, l) in indexfungorum.tokenize(name):
+                if m:
+                    print('m: %s line: %s' % (m, l))
+                else:
+                    print('m: None line: %s' % l)
+
+
+if __name__ == '__main__':
+    main()
---
+++ indexfungorum_authors_test.py
@@ -0,0 +1,26 @@
+import indexfungorum_authors
+import unittest
+
+class TestLTWA(unittest.TestCase):
+
+    def test_tokenize(self):
+        test_data = (
+            'Goossens-Fontana, J.A.A.    \n'
+            '(Stizenberger ex Arnold) R.C. Harris in Egan\n'
+            'Nees & T. Nees ex Link, Willdenow\n'
+            'Guzmán, Ram.-Guill.,\n'
+        )
+        expected = [
+            'Goossens', 'Fontana', 'J.A.A.',
+            'Stizenberger', 'ex', 'Arnold', 'R.C.', 'Harris', 'in', 'Egan',
+            'Nees', '&', 'T.', 'Nees', 'ex', 'Link', 'Willdenow',
+            'Guzmán', 'Ram.', 'Guill.',
+        ]
+        ifa = indexfungorum_authors.IndexFungorumAuthors()
+        
+        for (m, l) in ifa.tokenize(test_data):
+            print("DEBUG: m, l", m, l)
+            self.assertEqual(m, expected.pop(0))
+
+if __name__ == '__main__':
+    unittest.main()
