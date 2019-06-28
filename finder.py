c020f46adb5981152541670bce49d36dafc7d987
La Monte Henry Piggy Yarroll <piggy.yarroll@gmail.com>
2019-06-28 12:37:47 -0400

    Use the nomeclature regex to supplement Nomenclature recognition.
    We add a post-processing step to mark paragraphs as Nomenclature if
    they pass the regex. This seems to have a relatively low false
    positive rate.

--- finder.py
+++ finder.py
@@ -367,6 +367,10 @@ def define_args():
         help='Use paragraph boundaries as annotated, not the heuristic boundaries.',
         action='store_true')
     parser.add_argument(
+        '--insert_nomenclature',
+        help='Use regex to convert some paragraphs to Nomenclature.',
+        action='store_true')
+    parser.add_argument(
         '--csv',
         help='In test_classifiers_by_label, emit a csv.',
         action='store_true')
@@ -562,7 +566,10 @@ def main():
             text = str(pp)
             vectorize_text = vectorizer.transform([text])
             predict = classifier.predict(vectorize_text)[0]
+            if args.insert_nomenclature and pp.contains_nomenclature():
+                predict = 'Nomenclature'
             phase4.append(pp.replace_labels(labels=[Label(predict)]))
+
 
         if args.output_annotated:
             if not args.output_labels:
