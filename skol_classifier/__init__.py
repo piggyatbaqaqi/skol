388ebdd1b9bfa38c8a2b58d706cd4894c97c546d
La Monte Henry Piggy Yarroll <piggy.yarroll+github@gmail.com>
2025-11-16 17:01:59 -0500

    Remove references to CouchDBReader and CouchDBWriter.

--- skol_classifier/__init__.py
+++ skol_classifier/__init__.py
@@ -10,8 +10,6 @@ from .preprocessing import ParagraphExtractor, SuffixTransformer
 from .utils import get_file_list, create_evaluators, calculate_stats
 from .couchdb_io import (
     CouchDBConnection,
-    CouchDBReader,
-    CouchDBWriter,
 )
 
 __version__ = "0.1.0"
@@ -23,8 +21,6 @@ __all__ = [
     "create_evaluators",
     "calculate_stats",
     "CouchDBConnection",
-    "CouchDBReader",
-    "CouchDBWriter",
     "create_couchdb_reader",
     "create_couchdb_writer"
 ]
