c4c45431f42faa82ab5f2adc85ff881af6cc5a7e
La Monte Henry Piggy Yarroll <piggy.yarroll+github@gmail.com>
2026-01-20 14:36:45 -0500

    Draft About page. Claude set up the basic page based on the IST*/* documents, and I edited it for reality.

--- django/skolweb/urls.py
+++ django/skolweb/urls.py
@@ -10,5 +10,6 @@ urlpatterns = [
     path('api/', include('search.urls')),
     path('accounts/', include('accounts.urls')),
     path('contact/', include('contact.urls')),
+    path('about/', TemplateView.as_view(template_name='about.html'), name='about'),
     path('', TemplateView.as_view(template_name='index.html'), name='home'),
 ]
