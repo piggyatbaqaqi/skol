49929426a01687e71a75f92e96cf817fdad28b94
La Monte Henry Piggy Yarroll <piggy.yarroll+github@gmail.com>
2026-01-29 23:01:55 -0500

    webapp is dead code.

--- webapp/backend/api/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import API
-
-class APIAdmin(admin.ModelAdmin):
-    list_display = ('id', 'taxon', 'description', 'user')
-    search_fields = ('taxon', 'description', 'user__username')
-    list_filter = ('user',)
-
-admin.site.register(API, APIAdmin)
--- webapp/backend/api/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import API
-
-class APISerializer(serializers.ModelSerializer):
-    class Meta:
-        model = API
-        fields = '__all__'
-        read_only_fields = ('id', 'user')
